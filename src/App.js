2064885df758022214399a4d5d0c54e415580bca
Arun Vijayarengan <arunprabu.v@hotmail.com>
2021-05-18 09:49:48 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Shared/Header';
+import Footer from './components/Shared/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Posts from './components/Posts';
+import PostDetails from './containers/PostDetails';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        
+        {/* Header */} 
+       <Header />
+
+        <div className="mt-5">
+          <Switch>
+            <Route path='/' component={Home} exact/>
+            <Route path='/posts' component={Posts} exact/>
+            <Route path='/posts/1' component={PostDetails} />
+            <Route path='/about' component={About} />
+            {/* TODO: Learn about 404 */}
+          </Switch>
+        </div>
+
+        <Footer />
+
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/containers/PostList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+class PostList extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h3>Post List</h3>
+        <div className="list-group text-left">
+
+          <div className="list-group-item list-group-item-action">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">
+                <Link to={`posts/1`}>blog post 1</Link>
+              </h5>
+              <small>Post Id: 1</small>
+            </div>
+            <p className="mb-1">
+              lorem ipsum dolar sit
+            </p>
+          </div>
+
+          <div className='alert alert-warning'>
+            Post Not Found! You can add one.
+          </div>
+
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PostList;
